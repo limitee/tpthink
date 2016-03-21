b40c91606a219dc9f787dfaedc298b7b2646d155
liming <liming@admaster.com.cn>
2016-03-21 11:34:31 +0800

    game test use elog

--- game/src/main.rs
+++ game/src/main.rs
@@ -5,9 +5,15 @@ use game::base::Ticket;
 extern crate cons;
 use cons::ErrCode;
 
+#[macro_use]
+extern crate log;
+extern crate elog;
+
 fn main() {
+	let _ = elog::init();
 	let VF = ValidateFactory::new();
 	
+	info!("tests start...");
 	let ticket = Ticket::new(100, 10, 10, 1, 200, "01,02,03,04,05,06|01");
 	let rst = VF.validate(&ticket);
 	assert!(rst.is_ok());
@@ -23,4 +29,5 @@ fn main() {
 	assert!(rst.is_err());
 	assert!(rst.err().unwrap() == ErrCode::CountBtFive as i32);
 	
+	info!("all tests passed.");
 }
